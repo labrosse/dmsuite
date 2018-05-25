68c579349f51de11ba025166adaa45390270d82d
Stephane Labrosse <stephane@makatea.lst.ens-lyon.fr>
2018-05-25 10:52:44 +0200

    added several tests and corrected roots functions
    roots of Laguerre, Hermitte and Legendre polynomials are real
    and np.real is used to get rid of the 0 imaginary part.

---
+++ tests/test_lagroots.py
@@ -0,0 +1,8 @@
+import numpy as np
+import dmsuite as dm
+
+def test_chebdif4():
+    """ Test of Laguerre polynomials roots"""
+    expected = np.load('tests/data/lagroots10.npy')
+    computed = dm.lagroots(10)
+    assert np.all(computed == expected)
