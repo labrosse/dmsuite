5d94444ffaebec1f9c6dbe8be2be01588e253d1e
Adrien Morison <adrien.morison@gmail.com>
2023-02-26 01:04:10 +0000

    New non_poly_diff.Fourier class, impl DiffMatrices

--- dmsuite/non_poly_diff.py
+++ dmsuite/non_poly_diff.py
@@ -2,9 +2,89 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+from functools import cached_property
+
 import numpy as np
 from numpy.typing import NDArray
 from scipy.linalg import toeplitz
+
+from .poly_diff import DiffMatrices
+
+
+@dataclass(frozen=True)
+class Fourier(DiffMatrices):
+    """Fourier spectral differentiation matrices."""
+
+    nnodes: int
+
+    @cached_property
+    def nodes(self) -> NDArray:
+        return np.linspace(0.0, 2 * np.pi, self.nnodes, endpoint=False)
+
+    def at_order(self, order: int) -> NDArray:
+        # grid points
+        # grid spacing
+        dhh = 2 * np.pi / self.nnodes
+
+        nn1 = int(np.floor((self.nnodes - 1) / 2.0))
+        nn2 = int(np.ceil((self.nnodes - 1) / 2.0))
+        if order == 0:
+            # compute first column of zeroth derivative matrix, which is identity
+            col1 = np.zeros(self.nnodes)
+            col1[0] = 1
+            row1 = np.copy(col1)
+
+        elif order == 1:
+            # compute first column of 1st derivative matrix
+            col1 = 0.5 * np.array([(-1) ** k for k in range(1, self.nnodes)], float)
+            if self.nnodes % 2 == 0:
+                topc = 1 / np.tan(np.arange(1, nn2 + 1) * dhh / 2)
+                col1 = col1 * np.hstack((topc, -np.flipud(topc[0:nn1])))
+                col1 = np.hstack((0, col1))
+            else:
+                topc = 1 / np.sin(np.arange(1, nn2 + 1) * dhh / 2)
+                col1 = np.hstack((0, col1 * np.hstack((topc, np.flipud(topc[0:nn1])))))
+            # first row
+            row1 = -col1
+
+        elif order == 2:
+            # compute first column of 1st derivative matrix
+            col1 = -0.5 * np.array([(-1) ** k for k in range(1, self.nnodes)], float)
+            if self.nnodes % 2 == 0:
+                topc = 1 / np.sin(np.arange(1, nn2 + 1) * dhh / 2) ** 2.0
+                col1 = col1 * np.hstack((topc, np.flipud(topc[0:nn1])))
+                col1 = np.hstack((-np.pi**2 / 3 / dhh**2 - 1 / 6, col1))
+            else:
+                topc = (
+                    1
+                    / np.tan(np.arange(1, nn2 + 1) * dhh / 2)
+                    / np.sin(np.arange(1, nn2 + 1) * dhh / 2)
+                )
+                col1 = col1 * np.hstack((topc, -np.flipud(topc[0:nn1])))
+                col1 = np.hstack(([-np.pi**2 / 3 / dhh**2 + 1 / 12], col1))
+            # first row
+            row1 = col1
+
+        else:
+            # employ FFT to compute 1st column of matrix for order > 2
+            nfo1 = int(np.floor((self.nnodes - 1) / 2.0))
+            nfo2 = -self.nnodes / 2 * (order + 1) % 2 * np.ones((self.nnodes + 1) % 2)
+            mwave = 1j * np.concatenate(
+                (np.arange(nfo1 + 1), nfo2, np.arange(-nfo1, 0))
+            )
+            col1 = np.real(
+                np.fft.ifft(
+                    mwave**order
+                    * np.fft.fft(np.hstack(([1], np.zeros(self.nnodes - 1))))
+                )
+            )
+            if order % 2 == 0:
+                row1 = col1
+            else:
+                col1 = np.hstack(([0], col1[1 : self.nnodes + 1]))
+                row1 = -col1
+        return toeplitz(col1, row1)
 
 
 def fourdif(nfou: int, mder: int) -> tuple[NDArray, NDArray]:
@@ -37,67 +117,8 @@ def fourdif(nfou: int, mder: int) -> tuple[NDArray, NDArray]:
     S.C. Reddy, J.A.C. Weideman 1998.  Corrected for MATLAB R13
     by JACW, April 2003.
     """
-    # grid points
-    xxt = 2 * np.pi * np.arange(nfou) / nfou
-    # grid spacing
-    dhh = 2 * np.pi / nfou
-
-    nn1 = int(np.floor((nfou - 1) / 2.0))
-    nn2 = int(np.ceil((nfou - 1) / 2.0))
-    if mder == 0:
-        # compute first column of zeroth derivative matrix, which is identity
-        col1 = np.zeros(nfou)
-        col1[0] = 1
-        row1 = np.copy(col1)
-
-    elif mder == 1:
-        # compute first column of 1st derivative matrix
-        col1 = 0.5 * np.array([(-1) ** k for k in range(1, nfou)], float)
-        if nfou % 2 == 0:
-            topc = 1 / np.tan(np.arange(1, nn2 + 1) * dhh / 2)
-            col1 = col1 * np.hstack((topc, -np.flipud(topc[0:nn1])))
-            col1 = np.hstack((0, col1))
-        else:
-            topc = 1 / np.sin(np.arange(1, nn2 + 1) * dhh / 2)
-            col1 = np.hstack((0, col1 * np.hstack((topc, np.flipud(topc[0:nn1])))))
-        # first row
-        row1 = -col1
-
-    elif mder == 2:
-        # compute first column of 1st derivative matrix
-        col1 = -0.5 * np.array([(-1) ** k for k in range(1, nfou)], float)
-        if nfou % 2 == 0:
-            topc = 1 / np.sin(np.arange(1, nn2 + 1) * dhh / 2) ** 2.0
-            col1 = col1 * np.hstack((topc, np.flipud(topc[0:nn1])))
-            col1 = np.hstack((-np.pi**2 / 3 / dhh**2 - 1 / 6, col1))
-        else:
-            topc = (
-                1
-                / np.tan(np.arange(1, nn2 + 1) * dhh / 2)
-                / np.sin(np.arange(1, nn2 + 1) * dhh / 2)
-            )
-            col1 = col1 * np.hstack((topc, -np.flipud(topc[0:nn1])))
-            col1 = np.hstack(([-np.pi**2 / 3 / dhh**2 + 1 / 12], col1))
-        # first row
-        row1 = col1
-
-    else:
-        # employ FFT to compute 1st column of matrix for mder > 2
-        nfo1 = int(np.floor((nfou - 1) / 2.0))
-        nfo2 = -nfou / 2 * (mder + 1) % 2 * np.ones((nfou + 1) % 2)
-        mwave = 1j * np.concatenate((np.arange(nfo1 + 1), nfo2, np.arange(-nfo1, 0)))
-        col1 = np.real(
-            np.fft.ifft(
-                mwave**mder * np.fft.fft(np.hstack(([1], np.zeros(nfou - 1))))
-            )
-        )
-        if mder % 2 == 0:
-            row1 = col1
-        else:
-            col1 = np.hstack(([0], col1[1 : nfou + 1]))
-            row1 = -col1
-    ddm = toeplitz(col1, row1)
-    return xxt, ddm
+    fourier = Fourier(nnodes=nfou)
+    return fourier.nodes, fourier.at_order(mder)
 
 
 def sincdif(npol: int, mder: int, step: float) -> tuple[NDArray, NDArray]:
